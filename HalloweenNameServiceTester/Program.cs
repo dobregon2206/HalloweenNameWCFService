bc66e363f55f4eb48cfff05d27f2cfb45f617ceb
Naycoolh <61954566@pronabec.edu.pe>
2023-09-13 13:03:15 -0500

    Autenticacion Agregada

--- HalloweenNameServiceTester/Program.cs
+++ HalloweenNameServiceTester/Program.cs
@@ -16,8 +16,20 @@ namespace HalloweenNameServiceTester
         /// Main Method for Program Entry
         /// </summary>
         /// <param name="args">No input arguments used</param>
+     
+        static Dictionary<string, string> usuarios = new Dictionary<string, string>();
+
         static void Main(string[] args)
         {
+            RegistrarUsuario("Naycool", "12345");
+            RegistrarUsuario("usuario", "12345");
+
+            if (!AutenticarUsuario())
+            {
+                Console.WriteLine("Incorrecto");
+                return;
+            }
+
             // Create Client Proxy to the Service
             HalloweenServiceClient proxy = new HalloweenServiceClient();
 
@@ -36,7 +48,7 @@ namespace HalloweenNameServiceTester
             // Call the Service through the Proxy
             // FYI WCF/WSDL Does not Support Generics in Services
             // It returns an Array of Items 
-            List<Name> names = proxy.GetRandomNames(5).ToList<Name>();
+            List<Name> names = proxy.GetRandomNames(5).ToList();
 
             Console.WriteLine("List of Halloween Names");
             Console.WriteLine("=======================");
@@ -44,15 +56,39 @@ namespace HalloweenNameServiceTester
             foreach (Name item in names)
             {
                 Console.WriteLine("{0} {1}", item.FirstName, item.LastName);
-            } 
+            }
 
             #endregion Test Get List of Halloween Names
 
             // Wait for the User to See the Input
             Console.WriteLine("Press <Enter> to Quit...");
             Console.ReadLine();
+        }
+        static void RegistrarUsuario(string username, string password)
+        {
+            usuarios[username] = password;
+        }
 
-        } // end of main method
-    } // end of class
+        static bool AutenticarUsuario()
+        {
+            Console.WriteLine("Por favor, ingresa tus credenciales:");
 
-} // end of namespace
+            Console.Write("Nombre de usuario: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Contraseña: ");
+            string password = Console.ReadLine();
+
+            if (usuarios.ContainsKey(username) && usuarios[username] == password)
+            {
+                Console.WriteLine("Autenticación exitosa.");
+                return true;
+            }
+
+            Console.WriteLine("Autenticación fallida.");
+            return false;
+
+        }// end of main method
+    }// end of class
+
+} //end of namespace
